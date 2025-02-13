b3535e45e75e0ba39a7fd6e4a61a665d0323c0e2
joonghyunAn <anjoonghyun92@gmail.com>
2025-02-13 13:57:13 -0500

    dqr_code_update

--- project_1/StatsReport.py
+++ project_1/StatsReport.py
@@ -2,10 +2,16 @@ import pandas as pd
 class StatsReport:
  def __init__(self):
      self.statsdf = pd.DataFrame()
-     self.statsdf['stat'] = ['types','cardinality','mean', 'min', 'max']
+     self.statsdf['stat'] = ['types','cardinality','mean','median','stdev', 'min', 'max','nzero','nques','nmissing']
      pass
  def addCol(self, label, d):
-     self.statsdf[label] = [d.dtypes,d.nunique(),d.mean(), d.min(), d.max()]
+     zero_count = (d == 0).sum()
+     #question_count = d.apply(lambda x: str(x).count('?')).sum()
+     #missing_count = d.apply(lambda x: str(x).strip() == "").sum()
+     question_count = (d == "?").sum()
+     missing_count = (d =="").sum()
+     #question_count = d.str.contains('?').sum()
+     self.statsdf[label] = [d.dtypes,d.nunique(),d.mean(),d.median(), d.std(),d.min(), d.max(),zero_count,question_count,missing_count]
  def to_string(self):
      return self.statsdf.to_string()
  def writetofile(self, file_path,data):
